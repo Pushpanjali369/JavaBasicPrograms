9a2d63cae9fe848a6fd125d5717bd0061f63f00b
Pushpanjali369 <pushpanjali7761@gmail.com>
2026-01-21 13:45:28 +0530

    new dict

---
+++ loopProblems/1to100Number.java
@@ -0,0 +1,7 @@
+public class 1to100Number {
+    public static void main(){
+        
+    }
+
+    
+}
--- patternProblems/patternProblem.java
+++ patternProblems/patternProblem.java
@@ -5,36 +5,37 @@ import java.util.Scanner;
 public class patternProblem {
         public static void main(String[] args) {
             System.out.print("Enter the number of rows:");
-            Scanner input = new Scanner(System.in);
-            int n = input.nextInt();
-            // right trangle
+            try (Scanner input = new Scanner(System.in)) {
+                int n = input.nextInt();
+                // right trangle
 
-            // for(int i=1; i<=n; i++){
-            //     for(int j=1; j<=i; j++){
-            //         System.out.print("*");
-            //     }
-            //     System.out.println();
-            // }
+                // for(int i=1; i<=n; i++){
+                //     for(int j=1; j<=i; j++){
+                //         System.out.print("*");
+                //     }
+                //     System.out.println();
+                // }
 
-            // reverse right trangle
-            // for(int i=1; i<=n; i++){
-            //     for(int j=1; j<=n+1-i; j++){
-            //         System.out.print("*");
-            //     }
-            //     System.out.println();
-            // }
+                // reverse right trangle
+                // for(int i=1; i<=n; i++){
+                //     for(int j=1; j<=n+1-i; j++){
+                //         System.out.print("*");
+                //     }
+                //     System.out.println();
+                // }
 
-            for(int i=1; i<=n; i++){
-                // it is for number of spaces
-                for(int j=1; j<=n-i; j++){
-                    System.out.print(" ");
+                for(int i=1; i<=n; i++){
+                    // it is for number of spaces
+                    for(int j=1; j<=n-i; j++){
+                        System.out.print(" ");
+                    }
+                    // it is for star trangle
+                    for(int k=1; k<=i; k++){
+                        System.out.print("*");
+
+                    }
+                    System.out.println();
                 }
-                // it is for star trangle
-                for(int k=1; k<=i; k++){
-                    System.out.print("*");
-
-                }
-                System.out.println();
             }
 
         }
